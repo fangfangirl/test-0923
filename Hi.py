f36541c9eb79583e266ace7f21d0481e0ed76ae9
fangfangirlgit config --global user.name fangfangirl <fangfangirl77@gmail.com>
2026-09-23 19:46:10 +0800

    first

--- Hi.py
+++ Hi.py
@@ -1,1 +1,3 @@
-print("Hi")
+
+
+Fine-tuning large language models (LLMs) improves performance but introduces critical safety vulnerabilities: even minimal harmful data can severely compromise safety measures. We observe that perturbations orthogonal to the alignment direction - defined by weight differences between aligned (safe) and unaligned models - rapidly compromise model safety. In contrast, upd
